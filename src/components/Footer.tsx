a27d139cd6ecd78c5364d2edc9c0adaee1b22ddb
Akihiro Nishiya <akihiro.nishiya@gmail.com>
2019-05-26 23:02:16 +0900

    footer typography

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { FC, ClassAttributes, useContext } from 'react';
 import { context as StateContext } from '../context/state';
 import styled from '@emotion/styled';
+import { Typography } from '@material-ui/core/';
 
 interface Props {}
 
@@ -9,12 +10,15 @@ const Footer: FC<ClassAttributes<HTMLElement> & Props> = ({}) => {
   const theme = useContext(StateContext).state.theme;
   const Root = styled('div')({
     backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
     marginTop: theme.spacing(6),
     padding: theme.spacing(4),
   });
 
-  return <Root>footer</Root>;
+  return (
+    <Root>
+      <Typography color="textSecondary">footer</Typography>
+    </Root>
+  );
 };
 
 export default Footer;
