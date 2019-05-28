89e3dc53ee3004a30d3758ea4e189112b756c013
Akihiro Nishiya <akihiro.nishiya@gmail.com>
2019-05-28 16:07:43 +0900

    change to use ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,11 @@
-import wrapWithProvider from "./wrap-with-provider"
-export const wrapRootElement = wrapWithProvider
+import React from 'react';
+import wrapWithProvider from './wrap-with-provider';
+export const wrapRootElement = wrapWithProvider;
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <React.Fragment key="polyfill-script">
+      <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol" />
+    </React.Fragment>,
+  ]);
+};
