e864bc11001f61872bb41f3621d9e675d2dd95e7
Akihiro Nishiya <akihiro.nishiya@gmail.com>
2019-05-28 23:39:26 +0900

    change schedule top start date

--- gatsby-node.js
+++ gatsby-node.js
@@ -98,6 +98,6 @@ exports.onCreatePage = ({ page, actions }) => {
   if (page.path === '/schedule/') {
     const oldPage = Object.assign({}, page);
     deletePage(oldPage);
-    createPage({ ...page, context: { currentDate: format(current()) } });
+    createPage({ ...page, context: { currentDate: format(dateFns.startOfMonth(dateFns.addMonths(current(), -1))) } });
   }
 };
